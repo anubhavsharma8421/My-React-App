bfefd207a5814279430aa1aab1a3300528acf517
anubhavsharma8421 <anubhavsharma8421@gmail.com>
2024-01-13 14:52:19 +0530

    Changed d to D

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import logo from './logo.svg';
+
 import React from "react";
 import './App.css';
-import DemandNew from "./Modules/Demand/demandNew";
+import DemandNew from "./Modules/Demand/DemandNew";
 
 
 
